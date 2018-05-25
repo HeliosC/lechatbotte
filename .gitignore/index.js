992f8d03c32036db51cd374a3995bd8a58a1e866
HeliosC <36132864+HeliosC@users.noreply.github.com>
2018-05-25 19:12:39 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,1 +1,15 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
 
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === "pong") {
+    msg.reply("Ping!");
+	msg.channel.send("YO MON POTE");
+  }
+});
+
+client.login(process.env.TOKENchat);
