12557ac0c5454455906f4c1fb54d9170a34ffd02
HeliosC <36132864+HeliosC@users.noreply.github.com>
2018-05-26 00:08:45 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,4 +1,4 @@
-const Discord = require("discord.js");
+/*const Discord = require("discord.js");
 const client = new Discord.Client();
 
 client.on('ready', () => {
@@ -11,5 +11,78 @@ client.on('message', msg => {
 	msg.channel.send("YO MON POTE");
   }
 });
+
+client.login(process.env.TOKENchat);
+*/
+
+
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+const chanIm2 = "images_videos_trop_lentes";
+const chanIm = "images";
+
+
+// Extract the required classes from the discord.js module
+const { Client, MessageAttachment } = require('discord.js');
+
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+
+
+client.on('message', msg => {
+  if (msg.content === "ping") {
+    msg.reply("Ping!")
+	msg.channel.send("YOo")
+  }
+
+  //msg.channel.send( msg.channel.name)
+
+
+
+  /*if(msg.attachments === null){
+    console.log("ah") 
+  }else{
+    console.log("ok") 
+    //msg.channel.send( msg.attachments.map )  
+    for( var [key, value] of msg.attachments ){
+      //console.log(key + ' = ' + value)
+      msg.reply("reste tranquille")
+      //break
+    }
+    //client.channels.find('name','images').send(msg)
+
+  }*/
+
+  if(!msg.author.bot){
+    if(msg.channel.name != chanIm){
+        for( var [key, value] of msg.attachments ){
+            //client.channels.find('name',chanIm).send("0")
+            //client.channels.find('name',chanIm).send(""+msg.author+" : "+msg.content,{  file : msg.MessageAttachment })
+            //client.channels.find('name',chanIm).send("1")
+            client.channels.find('name',chanIm).send(""+msg.author+" : "+msg.content,{ file : value.proxyURL })
+            //client.channels.find('name',chanIm).send("2")
+            //client.channels.find(val => val.name === chanIm).send(""+msg.author+" : "+msg.content,{ file : value.proxyURL })
+            msg.channel.send(""+msg.author+" "+client.channels.find("name",chanIm))
+            //msg.channel.send(value.message)
+            //client.channels.find('name','images').send("lolilol")
+            //client.channels.find('name','images').send(msg.content,{   file: "https://www.sciencesetavenir.fr/assets/img/2017/03/29/cover-r4x3w1000-58dbbd655242b-capture-d-e-cran-2017-03-29-a-15-55-40.png"    })
+            //const attachment = new MessageAttachment('https://i.imgur.com/w3duR07.png');
+            //const attachment = new MessageAttachment(value.);
+            //msg.channel.send(`${msg.author},`, attachment)
+        }
+    msg.delete()
+    }}
+
+
+  /*if(!msg.author.bot){
+    console.log(msg.embeds.length)
+  }*/
+
+
+})
 
 client.login(process.env.TOKENchat);
