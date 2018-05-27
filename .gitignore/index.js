6f3cd52935b93a43a7de399e818828ce1961ab70
HeliosC <36132864+HeliosC@users.noreply.github.com>
2018-05-27 16:06:25 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,9 +1,19 @@
-/*const Discord = require("discord.js");
+const Discord = require("discord.js");
 const client = new Discord.Client();
+
+const chanIm = "images_videos_trop_lentes";
+//const chanIm2 = "images";
+
+
+// Extract the required classes from the discord.js module
+const { Client, MessageAttachment } = require('discord.js');
+
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
+
+
 
 client.on('message', msg => {
 	
