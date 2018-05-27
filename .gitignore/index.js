5054363f87d2c5f48e23c94f97a62f24bcdc2e05
HeliosC <36132864+HeliosC@users.noreply.github.com>
2018-05-27 13:23:52 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -98,7 +98,7 @@ client.on('message', msg => {
             //const attachment = new MessageAttachment(value.);
             //msg.channel.send(`${msg.author},`, attachment)
 
-	    //break
+	break
 
 	msg.delete()
 
