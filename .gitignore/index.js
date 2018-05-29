8ddf1eef0c804488bbe205e5d41852d59f81cd5d
HeliosC <36132864+HeliosC@users.noreply.github.com>
2018-05-29 17:37:52 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -4,8 +4,8 @@ const { Client, MessageAttachment } = require('discord.js')
 
 
 const chanIm = "images_videos_trop_lentes"
-//const chanCh = "test"
-const chanCh = "cest_ta_vie"
+const chanCh = "test"
+//const chanCh = "cest_ta_vie"
 const nombot = "Le Chat Botté"
 var nommodo = "🐾Chats sous chef"
 var nomadmin = "🦄Le Chat en chef"
@@ -16,29 +16,34 @@ client.on('ready', () => { console.log(`Logged in as ${client.user.tag}!`) })
 
 client.on('message', msg => {
 
-  if (msg.mentions.everyone) {
-    //msg.channel.send("wow")
-  } else {
-    for (user of client.users) {
-      if (user[1].username == nombot && user[1].bot && msg.isMemberMentioned(user[1])) {
-        msg.reply("reste tranquile")
-      }
-    }
-  }
+
 
   if (!msg.author.bot) {
 
 
     let modRole = msg.guild.roles.find("name", nommodo);
     let adminRole = msg.guild.roles.find("name", nomadmin);
-    if (msg.channel.name == chanCh && !(msg.member.roles.has(modRole.id) || msg.member.roles.has(adminRole.id))) {
-      for (var [key, value] of msg.attachments) {
-        client.channels.find('name', chanIm).send("" + msg.author + " : " + msg.content)
-        client.channels.find('name', chanIm).send({ file: value.proxyURL })
-        msg.channel.send("" + msg.author + " " + client.channels.find("name", chanIm))
-        setTimeout(suiteTraitement, 500)
-        function suiteTraitement() { msg.delete() }
-        break
+    if (!(msg.member.roles.has(modRole.id) || msg.member.roles.has(adminRole.id))) {
+
+      if (msg.mentions.everyone) {
+        //msg.channel.send("wow")
+      } else {
+        for (user of client.users) {
+          if (user[1].username == nombot && user[1].bot && msg.isMemberMentioned(user[1])) {
+            msg.reply("reste tranquile")
+          }
+        }
+      }
+
+      if (msg.channel.name == chanCh) {
+        for (var [key, value] of msg.attachments) {
+          client.channels.find('name', chanIm).send("" + msg.author + " : " + msg.content)
+          client.channels.find('name', chanIm).send({ file: value.proxyURL })
+          msg.channel.send("" + msg.author + " " + client.channels.find("name", chanIm))
+          setTimeout(suiteTraitement, 500)
+          function suiteTraitement() { msg.delete() }
+          break
+        }
       }
     }
 
@@ -54,5 +59,4 @@ client.on('message', msg => {
 
 
 })
-
 client.login(process.env.TOKENchat);
