b2da8c7c3d951cdf5b5192bef14e8a248ad2da29
HeliosC <36132864+HeliosC@users.noreply.github.com>
2018-06-14 21:37:18 +0200

    Update motrigolo.js

--- .gitignore/Packages/bddquiz/motrigolo.js
+++ .gitignore/Packages/bddquiz/motrigolo.js
@@ -1,152 +1,83 @@
 var str = "\
 (1)\
-(Q)Les volcans peuvent cracher des cendres à plus de 30 km (18 mi) dans les airs.\
-(R)La réponse est VRAI. Les volcans peuvent cracher des cendres à plus de 30 km (18 mi) dans l’atmosphère.\
+(Q)Lorsque les tortues entrent la tête et les pattes dans leur carapace, on dit qu’elles se carapatent.\
+(R)La réponse est FAUX. Le verbe pronominal se carapater signifie « s’enfuir ».\
 (2)\
-(Q)La Terre pèse environ 6 000 000 000 000 000 000 000 000 kg (13,2 x 1024 lb).\
-(R)La réponse est VRAI. Le poids de notre planète est évalué à 6 x 1024 kg (13,2 x 1024 lb).\
+(Q)Tartempion est le surnom de Louis VI, dit « le Gros » ou « Tartempion » (1081-1137), de la dynastie des Capétiens.\
+(R)La réponse est FAUX. Tartempion est un nom qui désigne une personne quelconque. Le surnom de Louis VI était « le Gros » ou « le Batailleur ».\
 (3)\
-(Q)Il y a possiblement 20 volcans en activité en ce moment.\
-(R)La réponse est VRAI. Il y a entre 50 et 70 volcans actifs (en éruption) chaque année. Comme les périodes d’éruptions s’échelonnent sur plusieurs semaines, voire plusieurs mois, on peut déduire qu’une vingtaine de volcans sont en activité à tout moment.\
+(Q)Panégyrique est le nom donné aux éléphants durant la Première Guerre punique (264 à 241 av. J.-C.).\
+(R)La réponse est FAUX. Le mot panégyrique signifie « discours, texte à la louange d’un personnage célèbre, d’une nation, d’une cité ». C’est un éloge sans réserve, emphatique.\
 (4)\
-(Q)Les océans couvrent 45 % de la planète.\
-(R)La réponse est FAUX. Les océans couvrent plus de 70 % de la planète, soit 361 millions km² des 510 millions de km² (140 millions mi² des 197 millions mi²) de la surface totale de la planète.\
+(Q)Le mot tarabiscoté est bien utilisé dans : « L’intrique de la pièce de théâtre était tarabiscotée. Je n’ai rien compris. »\
+(R)La réponse est VRAI. L’adjectif tarabiscoté signifie, entre autres, « alambiqué, tiré par les cheveux, compliqué pour rien ». L’intrigue d’une pièce de théâtre peut donc être tarabiscotée.\
 (5)\
-(Q)Le volcan Mauna Kea à Hawaï peut être considéré comme plus haut que l’Everest.\
-(R)La réponse est VRAI. Si nous mesurons le volcan Mauna Kea à partir du plancher océanique où il repose, le volcan atteint 10 203 m (33 474 pi) de hauteur. Le mont Everest est haut de « seulement » 8848 m (29 028 pi).\
+(Q)On appelait ouistitis pontificaux les soldats du pape durant la guerre contre les troupes royales italiennes (1860-1871).\
+(R)La réponse est FAUX. Le nom donné aux soldats du pape durant la guerre contre les troupes royales italiennes (1860-1871) est zouave. Le ouistiti est un petit singe d’Amérique tropicale, à fortes griffes, à longue queue et aux oreilles touffues.\
 (6)\
-(Q)La Terre est la quatrième planète la plus près du Soleil.\
-(R)La réponse est FAUX. La Terre est la troisième planète la plus près du Soleil. En ordre de proximité du Soleil, les planètes sont : Mercure, Vénus, Terre, Mars, Jupiter, Saturne, Uranus et Neptune.\
+(Q)Ventripotent est le nom donné au sommet arrondi des vagues en haute mer.\
+(R)La réponse est FAUX. L’adjectif ventripotent signifie « qui a un gros ventre ».\
 (7)\
-(Q)On trouve plus de 25 000 îles dans l’océan Pacifique.\
-(R)La réponse est VRAI. On trouve plus de 25 000 îles dans l’océan Pacifique; c’est plus que toutes les îles dans les 4 autres océans réunis.\
+(Q)Le verbe yoyoter signifie « jouer au yoyo ».\
+(R)La réponse est FAUX. Le verbe yoyoter signifie « perdre la tête, divaguer ».\
 (8)\
-(Q)La chute Saut de l’ange est si haute que durant les journées chaudes, l’eau s’évapore avant de toucher la rivière.\
-(R)La réponse est VRAI. Durant les journées les plus chaudes de l’année, l’eau se transforme en brouillard avant d’atteindre la rivière en bas.\
+(Q)Le mot calembredaine désigne une bedaine qui donne une allure un peu ridicule.\
+(R)La réponse est FAUX. Le mot calembredaine signifie « propos fantaisiste, extravagant ». Par exemple : « Mon grand-père aime nous raconter des calembredaines, ce qui enrage ma grand-mère. »\
 (9)\
-(Q)La longueur de l’orbite terrestre est de 930 millions km (577 millions mi).\
-(R)La réponse est VRAI. La longueur de l’orbite terrestre est de 930 millions km (577 millions mi).\
+(Q)Être patraque signifie « ne pas se sentir bien ».\
+(R)La réponse est VRAI. Utilisé comme nom, le mot patraque désigne une personne faible, qui a l’air malade. En tant qu’adjectif, il veut dire « qui ne se sent pas bien ».\
 (10)\
-(Q)Le lac Titicaca est le plus grand lac d’eau douce d’Afrique.\
-(R)La réponse est FAUX. Avec sa superficie de 8372 km² (3232 mi²), le lac Titicaca est le plus grand lac d’eau douce d’Amérique du Sud.\
+(Q)Le mot vernaculaire est bien utilisé dans : « J’ignore pourquoi, mais les longues pattes vernaculaires des araignées me dégoûtent. »\
+(R)La réponse est FAUX. On dit qu’un nom est vernaculaire lorsque c’est le nom d’un animal ou d’une plante dans la langue courante. Par exemple, « épeire diadème » est le nom vernaculaire de l’Araneus diadematus.\
 (11)\
-(Q)La distance entre le Soleil et la Terre est de 88 millions km (54,7 millions mi).\
-(R)La réponse est FAUX. La distance entre le Soleil et la Terre est d’environ 147 millions de kilomètres (91,34 millions de miles) au mois de janvier, et 152 millions de kilomètres (94,44 millions de miles) au mois de juillet.\
+(Q)Ornithorynque est le nom donné à un mammifère amphibie à bec corné et aplati.\
+(R)La réponse est VRAI. L’ornithorynque est un mammifère d’Australie et de Tasmanie. Amphibie et ovipare, il a un bec corné et aplati comme celui d’un canard, des pattes palmées munies de griffes et une longue queue plate.\
 (12)\
-(Q)La rotation de la Terre s’accélère.\
-(R)La réponse est FAUX. La rotation de la Terre ralentit d’environ 1,7 milliseconde par cent ans.\
+(Q)Estaminet désigne une race de chat sans poils.\
+(R)La réponse est FAUX. Un estaminet est un petit café, un débit de boisson populaire. La race de chat sans poils se nomme sphynx.\
 (13)\
-(Q)Le désert du Sahara est plus petit que le Mexique.\
-(R)La réponse est FAUX. Avec une superficie de 9,4 millions km² (3 629 360 mi²), le Sahara est environ 4,5 fois plus vaste que le Mexique, dont la superficie est de 1,97 million km² (760 621 mi²).\
+(Q)Crapahuter, c’est conduire un traîneau à chiens.\
+(R)La réponse est FAUX. Crapahuter, c’est progresser dans un terrain difficile, accidenté. Par exemple, « les biologistes crapahutent depuis trois jours dans la forêt boueuse ».\
 (14)\
-(Q)L’océan Atlantique est le plus grand océan de la Terre.\
-(R)La réponse est FAUX. L’océan Atlantique a une superficie d’environ 100 000 000 km² (38 610 215 mi²). C’est l’océan Pacifique avec ses 165 000 000 km² (63 700 000 mi²) qui est le plus vaste des océans du globe.\
+(Q)L’anacoluthe est un parasite de l’estomac.\
+(R)La réponse est FAUX. L’anacoluthe est une figure stylistique qui sert à marquer la discontinuité, la rupture dans la syntaxe d’une phrase, mais sans rupture du lien logique. Par exemple, « Le nez de Cléopâtre, s’il eût été plus court, toute la face de la terre aurait changé » —Pascal. (Le parasite présenté sur l’image est un Helicobacter pylori.)\
 (15)\
-(Q)En moyenne, il y a 5 éclairs à la seconde sur la planète.\
-(R)La réponse est VRAI. En moyenne, la Terre est soumise à 5 éclairs à la seconde.\
+(Q)Le zygomatique est une région du cerveau qui régule divers sons, dont le rire.\
+(R)La réponse est FAUX. Les zygomatiques sont des muscles qui s’étendent de la pommette à la commissure des lèvres et qui entrent en jeu lors du rire ou du sourire.\
 (16)\
-(Q)La Lune complète un tour de la Terre en un peu plus de 27 jours.\
-(R)La réponse est VRAI. L’orbite de la Lune autour de la Terre dure 27 jours 7 heures et 43 minutes.\
+(Q)Cacatoès est le nom donné à une variété d’arachides du Brésil.\
+(R)La réponse est FAUX. Cacatoès est le nom d’un oiseau grimpeur d’Australie, d’Indonésie et de Nouvelle-Guinée. Il est voisin du perroquet, a un plumage coloré et une grande huppe érectile.\
 (17)\
-(Q)Les déserts de sel sont en fait des déserts de roches blanches.\
-(R)La réponse est FAUX. Les déserts de sel, comme le Salinas Grandes à Jujuy en Argentine, sont véritablement couverts de sel.\
+(Q)Le patchouli est une plante aromatique.\
+(R)La réponse est VRAI. Le patchouli est une plante aromatique originaire d’Asie, qui fournit une essence très parfumée.\
 (18)\
-(Q)Le rayonnement solaire prend environ 8 secondes pour atteindre la Terre.\
-(R)La réponse est FAUX. Il faut environ 8 minutes pour que le rayonnement solaire atteigne la Terre.\
+(Q)Cet énoncé a du sens : « J’ai obtenu l’antépénultième place dans la course. C’est mieux que l’an dernier où j’avais terminé avant-dernier. »\
+(R)La réponse est VRAI. L’adjectif antépénultième signifie « qui précède immédiatement l’avant-dernier ». L’énoncé est juste si le coureur s’est classé devant l’avant-dernier, c’est-à-dire avant celui qui occupait le pénultième rang.\
 (19)\
-(Q)La température à la surface des océans varie de −2 °C (28,4 °F) à 35 °C (95 °F).\
-(R)La réponse est VRAI. Selon la NASA, la température de la surface des océans varie de −2 °C (28,4 °F) dans les océans Arctique et Antarctique à 35 °C (95 °F) dans l’océan Indien. Le point de congélation de l’eau salée est inférieur à celui de l’eau douce. Dans l'océan, les premiers cristaux de glace apparaissent à -1,9 °C (28,58 °F).\
+(Q)Le kouglof est une brioche en forme de couronne.\
+(R)La réponse est VRAI. Le kouglof est une brioche en forme de couronne et garnie de raisins secs. Cette pâtisserie est une spécialité alsacienne.\
 (20)\
-(Q)La terre est secouée par environ 500 000 tremblements de terre par année.\
-(R)La réponse est VRAI. Il y a environ 500 000 tremblements de terre chaque année sur la Terre. Environ 400 000 d’entre eux passent inaperçus aux humains, et une centaine peuvent causer des dégâts matériels.\
+(Q)Le nyctalope est une petite antilope d’Afrique.\
+(R)La réponse est FAUX. Un nyctalope est un individu affecté par la nyctalopie, c’est-à-dire la faculté de voir la nuit, normale pour certains animaux et anormale pour les humains.\
 (21)\
-(Q)La plupart des éclairs ont lieu dans les régions polaires.\
-(R)La réponse est FAUX. Environ 70 % des éclairs se produisent dans les régions tropicales. Ils frappent rarement les régions polaires.\
+(Q)Le zinzolin est un instrument à vent, ancêtre du trombone.\
+(R)La réponse est FAUX. Le zinzolin est une couleur d’un violacé rougeâtre obtenue à partir de la semence de sésame. L’ancêtre du trombone s’appelle sacquebute (aussi saquebute, saqueboute).\
 (22)\
-(Q)La terre se déplace à une vitesse orbitale de 29,85 km (18,54 mi) par seconde.\
-(R)La réponse est VRAI. La vitesse orbitale moyenne de la Terre est d’environ 29,8 km/s (18,54 mi/s), ou 107 000 km/h (66 486 mi/h).\
+(Q)Le mot tintouin veut aussi dire « souci, inquiétude ».\
+(R)La réponse est VRAI. Le mot tintouin a deux significations. Il est synonyme de « vacarme » : ma nouvelle voisine fait un tintouin terrible. Et il veut aussi dire « souci, inquiétude » : elle se fait du tintouin pour rien.\
 (23)\
-(Q)Les chutes Victoria s’étendent sur une largeur de plus de 1,7 km (1,06 mi).\
-(R)La réponse est VRAI. Les chutes Victoria, à la frontière de la Zambie et du Zimbabwe, sont les plus larges du monde. Elles s’étendent sur plus de 1,7 km (1,06 mi).\
+(Q)Les borborygmes sont les onomatopées dans les bandes dessinées.\
+(R)La réponse est FAUX. Les borborygmes sont des gargouillements produits dans l’intestin ou l’estomac, ou des paroles inintelligibles, mal articulées. Les onomatopées sont des mots qui évoquent un bruit particulier. Par exemple, bang est l’onomatopée qui désigne une explosion, tant dans les bandes dessinées que dans d’autres textes.\
 (24)\
-(Q)À l’équateur, la vitesse de la rotation de la Terre sur elle-même est de 1670 km/h (1037 mi/h).\
-(R)La réponse est VRAI. À l’équateur, la vitesse de la rotation de la Terre sur elle-même est de 1670 km/h (1037 mi/h), soit 465 m/s (1525 pi/s).\
+(Q)Les cœlacanthes sont un genre de poissons osseux marins primitifs.\
+(R)La réponse est VRAI. Les cœlacanthes sont un genre de poissons osseux marins primitifs (fossiles, à l’exception d’une espèce toujours vivante) dont les nageoires sont des ébauches des membres des tétrapodes.\
 (25)\
-(Q)L’océan Indien est l’océan le plus chaud.\
-(R)La réponse est VRAI. L’océan Indien est l’océan le plus chaud de la Terre. La température de l’eau à sa surface peut dépasser les 30 °C (86 °F) à certains endroits, mais sa température de surface moyenne est de 22 °C (71,6 °F).\
+(Q)Le clafoutis est le bruit d’impact de petites vagues sur une embarcation.\
+(R)La réponse est FAUX. Le clafoutis est un gâteau cuit au four constitué d’une pâte de flan garnie de fruits.\
 (26)\
-(Q)La température de la lave varie entre 700 et 1200 °C (1292 et 2192 °F).\
-(R)La réponse est VRAI. Lors d’une éruption, la température de la lave varie entre 700 et 1200 °C (1292 et 2192 °F).\
+(Q)Le fandango est une danse espagnole d’un rythme rapide.\
+(R)La réponse est VRAI. Le fandango est une danse espagnole d’un rythme rapide, accompagnée par la guitare et les castagnettes.\
 (27)\
-(Q)La Grande Barrière de corail est visible de l’espace.\
-(R)La réponse est VRAI. La Grande Barrière de corail, qui longe la côte nord-est du Queensland, en Australie, s’étend sur 2600 km (1615 mi). Elle couvre une zone d’environ 344 000 km² (132 819 mi²). C’est le seul organisme vivant visible de l’espace.\
-(28)\
-(Q)Près des quatre cinquièmes de l’eau douce se trouvent sous forme de glace ou de neige.\
-(R)La réponse est VRAI. Près des quatre cinquièmes, de l’eau douce de la terre sont emprisonnés dans les glaciers et les neiges éternelles.\
-(29)\
-(Q)Les forêts couvrent environ 70 % des terres émergées.\
-(R)La réponse est FAUX. Les forêts couvrent environ 30 % des terres émergées.\
-(30)\
-(Q)Le noyau de la Terre a une température d’environ 6000 °K (5727 °C / 10 340 °F).\
-(R)La réponse est VRAI. Les scientifiques ont évalué que la température du centre le la Terre était d’environ 6000 °K (5727 °C / 10 340 °F).\
-(31)\
-(Q)Un tiers des terres émergées sont partiellement ou totalement désertiques.\
-(R)La réponse est VRAI. Un tiers des terres émergées sont partiellement ou totalement désertiques. Et cette proportion augmente chaque année.\
-(32)\
-(Q)L’océan Atlantique s’agrandit chaque année.\
-(R)La réponse est VRAI. Les plaques tectoniques de l’Atlantique s’écartent lentement  : l’Europe et l’Afrique s’éloignent des Amériques d’environ 2 cm (0,79 po) chaque année.\
-(33)\
-(Q)Un éclair élève la température de l’air à 30 000 °C (54 000 °F).\
-(R)La réponse est VRAI. La durée totale d’un éclair n’est que d’environ un quart de seconde, mais cela suffit pour porter très rapidement la température de l’air à 30 000 °C (54 000 °F).\
-(34)\
-(Q)La Terre ne peut pas contenir plus de 8 fois la Lune.\
-(R)La réponse est FAUX. La Terre pourrait contenir 36 Lunes complètes, et 39 si on pouvait briser les sphères en morceaux pour remplir les espaces entre elles.\
-(35)\
-(Q)L’Arctique est plus froid que l’Antarctique.\
-(R)La réponse est FAUX. L’Antarctique est plus froid que l’Arctique. La température moyenne de l’Antarctique est -60 °C (-76 °F) en hiver et de -28,2 °C (-18,8 °F) durant l’été. La température moyenne de l’Arctique est -40 °C (-40 °F) en hiver et 0 °C (32 °F) en été.\
-(36)\
-(Q)La Terre est plus grosse que Mars.\
-(R)La réponse est VRAI. Le diamètre de la Terre à l’équateur est de 12 738 km (7915 mi), tandis que celui de Mars est de 6779 km (4212 mi).\
-(37)\
-(Q)La quantité moyenne annuelle de pluie tombant sur la Terre est d’environ 100 cm (39 po).\
-(R)La réponse est VRAI. La moyenne de précipitations annuelles sur l’ensemble de la surface de la Terre est d’environ 100 cm (39 po).\
-(38)\
-(Q)Le cratère « Meteor » de 12 km (7,5 mi) de diamètre s’est formé à la suite de l’écrasement d’une météorite de 50 m (165 pi) de diamètre.\
-(R)La réponse est VRAI. Situé en Arizona, le cratère « Meteor », qui a 12 km (7,5 mi) de diamètre et 190 m (623 pi) de profondeur, s’est formé à la suite de l’écrasement d’une météorite de 50 m (165 pi) de diamètre.\
-(39)\
-(Q)Le débit des chutes du Niagara est d’environ 100 000 litres (26 400 gallons US) à la seconde.\
-(R)La réponse est FAUX. Le débit des chutes du Niagara (parties canadienne et américaine combinées) est de 2 800 000 litres (740 000 gallons US) à la seconde.\
-(40)\
-(Q)À cause de son atmosphère, la Terre est plus chaude que Vénus (qui est pourtant plus près du Soleil).\
-(R)La réponse est FAUX. Venus est beaucoup plus chaude que la Terre. Sa température moyenne de 462 °C (863 °F) est assez chaude pour faire fondre le plomb.\
-(41)\
-(Q)Le désert d’Atacama au Chili est l’endroit le plus sec de la Terre.\
-(R)La réponse est VRAI. Le désert d’Atacama au Chili est l’endroit le plus sec de la Terre. Il y tombe en moyenne moins de 15 mm (0,59 po) de pluie par année. Certaines zones n’ont pas reçu de pluie depuis que nous faisons des relevés météorologiques.\
-(42)\
-(Q)La plupart des volcans se trouvent au centre des plaques tectoniques.\
-(R)La réponse est FAUX. La plupart des volcans se trouvent à la jonction des plaques tectoniques. Particulièrement dans la Ceinture de feu du Pacifique où se trouvent 75 % des volcans du monde.\
-(43)\
-(Q)L’Asie couvre environ 30 % des terres émergées de la planète.\
-(R)La réponse est VRAI. L’Asie est le plus grand continent de la Terre. Il couvre environ 30 % des terres émergées de la planète.\
-(44)\
-(Q)Les couleurs d’un arc-en-ciel apparaissent toujours dans le même ordre.\
-(R)La réponse est VRAI. Les couleurs d’un arc-en-ciel apparaissent toujours dans le même ordre, qui est, du sommet à la base : rouge, orange, jaune, vert, bleu, indigo et violet.\
-(45)\
-(Q)La profondeur moyenne des océans est d’environ 4250 m (14 000 pi).\
-(R)La réponse est VRAI. La profondeur moyenne des océans est d’environ 4250 m (14 000 pi).\
-(46)\
-(Q)L’océan Pacifique couvre environ un tiers de la planète.\
-(R)La réponse est VRAI. L’océan Pacifique couvre environ un tiers de la surface totale de la Terre, soit environ 165 000 000 km² (63 800 000 mi²).\
-(47)\
-(Q)L’Afrique couvre 14 % de la superficie de la Terre.\
-(R)La réponse est FAUX. Le continent africain couvre 6 % de la surface terrestre, et 20 % de la surface des terres émergées.\
-(48)\
-(Q)Certaines régions d’Amazonie reçoivent 1016 cm (400 po) de pluie chaque année.\
-(R)La réponse est VRAI. Certaines régions d’Amazonie reçoivent 1016 cm (400 po) de pluie chaque année, soit une moyenne de 3 cm (1,18 po) par jour.\
-(49)\
-(Q)L’eau salée des océans ne peut pas geler.\
-(R)La réponse est FAUX. L’eau salée peut geler, mais à une température inférieure à celle de l’eau douce qui gèle à 0 °C (32 °F); l’eau de mer commence à geler -2,0 °C (28,4 °F). Plus il y a de sel dans l’eau, plus bas est le point de congélation. Toutefois, quand l’eau de mer gèle, la glace contient très peu de sel, car seule l’eau se transforme en glace.\
-(50)\
 "
 
 exports.str=str
