746517826bb1b311aec5eac5769cb4e58d06de5b
HeliosC <36132864+HeliosC@users.noreply.github.com>
2018-06-14 21:08:34 +0200

    Add files via upload

---
+++ .gitignore/Packages/bddquiz/quebec.js
@@ -0,0 +1,83 @@
+var str = "\
+(1)\
+(Q)Être habillé comme la chienne à Jacques veut dire « être mal vêtu ».\
+(R)La réponse est VRAI. L’expression être habillé comme la chienne à Jacques signifie « être mal vêtu, porter des vêtements mal assortis ». L’expression existe aussi sous la variante être attriqué comme la chienne à Jacques.\
+(2)\
+(Q)Avoir une montée de lait est synonyme d’« avoir une montée de rage ».\
+(R)La réponse est VRAI. L’expression québécoise avoir (ou faire) une montée de lait signifie « avoir un accès de mauvaise humeur (généralement sans justification réelle), exprimer son indignation ou s’énerver fortement ».\
+(3)\
+(Q)Accrocher ses patins, c’est prendre sa retraite.\
+(R)La réponse est VRAI. Le hockey est le sport le plus populaire au Canada. De nombreuses expressions québécoises y font donc allusion. L’expression accrocher ses patins signifie « prendre sa retraite ».\
+(4)\
+(Q)Avoir les yeux gros comme des trous de suce (tétine) signifie « avoir le regard fixe ».\
+(R)La réponse est FAUX. L’expression avoir les yeux gros comme des trous de suce signifie « avoir des yeux à peine ouverts en raison de la fatigue ». Au Canada, le mot suce est synonyme de « tétine, ou sucette, pour bébé ».\
+(5)\
+(Q)Être une grosse bolle, c’est être naïf.\
+(R)La réponse est FAUX. L’expression être une grosse bolle signifie « être très doué, intelligent ou calé dans un domaine ».\
+(6)\
+(Q)L’expression tire-toi une bûche sert à inviter quelqu’un à se joindre à notre table.\
+(R)La réponse est VRAI. L’expression tire-toi une bûche est utilisée lorsque nous voulons inviter une personne à s’asseoir pour se joigne au groupe ou à s’installer à table avec les autres invités.\
+(7)\
+(Q)Quelqu’un qui swigne dans le beurre danse mal.\
+(R)La réponse est FAUX. L’expression franglaise swigner dans le beurre n’a aucun lien avec la danse. Elle signifie « donner un coup d’épée dans l’eau, manquer son coup ».\
+(8)\
+(Q)Lorsqu’on dit « accouche, qu’on baptise! », on demande sèchement à quelqu’un de se dépêcher.\
+(R)La réponse est VRAI. L’expression accouche, qu’on baptise! est une façon très directe et impatiente de demander à quelqu’un de se dépêcher.\
+(9)\
+(Q)L’expression avoir des fonds de bouteille fait référence à de sérieux problèmes d’alcool.\
+(R)La réponse est FAUX. L’expression avoir des fonds de bouteille signifie « porter des lunettes épaisses ».\
+(10)\
+(Q)Frapper son Waterloo signifie « connaître un échec ».\
+(R)La réponse est VRAI. L’expression il a frappé son Waterloo est la traduction de l’expression anglaise He met his Waterloo. Elle signifie « connaître un échec, rencontrer un obstacle infranchissable » et fait référence à la défaite de Napoléon à Waterloo.\
+(11)\
+(Q)Être fou comme un balai est une expression ironique pour décrire une personne réservée, timide.\
+(R)La réponse est FAUX. L’expression être fou comme un balai décrit quelqu’un qui ne tient pas en place, qui est excité, agité.\
+(12)\
+(Q)Être gelé peut aussi vouloir dire « être sous l’influence de la drogue ».\
+(R)La réponse est VRAI. Être gelé signifie bien sûr « souffrir du froid », mais aussi « être sous l’influence de la drogue, être défoncé ».\
+(13)\
+(Q)Pelleter des nuages, c’est avoir des projets irréalisables.\
+(R)La réponse est VRAI. L’expression pelleter des nuages signifie « rêver sans tenir compte des contraintes réelles ». Un pelleteux de nuages, c’est un idéaliste ou un enthousiaste dépourvu de sens pratique, qui caresse des chimères.\
+(14)\
+(Q)L’expression oreilles de Christ sert à décrire les gens qui écoutent aux portes pour dénicher des ragots.\
+(R)La réponse est FAUX. La locution oreilles de Christ, que l’on prononce « oreilles de crisse », désigne la couenne de lard grillée dans une poêle ou frite dans l’huile et servie sous forme de croustilles épaisses.\
+(15)\
+(Q)Se faire dorer la couenne signifie « se faire bronzer ».\
+(R)La réponse est VRAI. La couenne n’est pas seulement la croûte tendre de quelque chose, ou de la peau de porc flambée. Dans l’expression se faire dorer la couenne, le mot couenne remplace le mot peau. Se faire dorer la couenne signifie donc « se faire bronzer ».\
+(16)\
+(Q)S’enfarger dans les fleurs du tapis, c’est marcher délicatement sur la pointe des pieds.\
+(R)La réponse est FAUX. L’expression s’enfarger dans les fleurs du tapis signifie « se laisser arrêter par de menus obstacles ou par des difficultés imaginaires ». Le verbe s’enfarger est synonyme de « trébucher ».\
+(17)\
+(Q)Ça ne prend pas la tête à Papineau signifie qu’une chose est facile à comprendre.\
+(R)La réponse est VRAI. L’expression ça ne prend pas la tête à Papineau signifie qu’une chose est facile à comprendre ou qu’une personne aurait dû réussir facilement la tâche qui lui était attribuée. Louis-Joseph Papineau, qui a mené les patriotes durant la rébellion de 1837, était un politicien reconnu pour sa grande intelligence.\
+(18)\
+(Q)L’expression ferme ta boîte sert à demander à quelqu’un de faire ses valises et de partir.\
+(R)La réponse est FAUX. L’expression ferme ta boîte est synonyme de « tais-toi, boucle-la, ferme-la ».\
+(19)\
+(Q)Avoir de l’eau dans sa cave, c’est porter des pantalons trop courts.\
+(R)La réponse est VRAI. L’expression avoir de l’eau dans sa cave signifie « porter des pantalons trop courts ». Elle fait référence au fait qu’on retrousse le bas de ses pantalons pour ne pas les mouiller lorsque la cave est inondée et qu’on doit réparer les dégâts.\
+(20)\
+(Q)Chauffer son char, c’est réchauffer sa voiture durant les froids d’hiver.\
+(R)La réponse est FAUX. L’expression chauffer son char signifie simplement « conduire sa voiture ». En effet, chauffer est parfois synonyme de « conduire » et le char québécois est habituellement une automobile, et non pas un char d’assaut ou un char allégorique. \
+(21)\
+(Q)Un petit boss des bécosses est un petit chef autoritaire et sans envergure.\
+(R)La réponse est VRAI. Pour bien comprendre cette expression, il faut d’abord savoir que le mot bécosse est probablement né d’une déformation de la locution anglaise « back house » qui se traduit littéralement par « maison à l’arrière  ». Ce terme désignait autrefois les latrines. Un petit boss des bécosses est donc un petit chef autoritaire et sans envergure.\
+(22)\
+(Q)Avoir passé la nuit sur la corde à linge signifie « avoir dormi en sous-vêtements ».\
+(R)La réponse est FAUX. Avoir passé la nuit sur la corde à linge signifie « avoir passé une mauvaise nuit, avoir mal ou peu dormi ». On demande à quelqu’un s’il a passé la nuit sur la corde à linge pour dire qu’il a les traits tirés, qu’il ne semble pas dans son assiette.\
+(23)\
+(Q)Changer 4 trente sous pour une piastre signifie « se faire avoir dans une arnaque ».\
+(R)La réponse est FAUX. L’expression changer 4 trente sous pour une piastre signifie « faire des changements qui n’apportent rien, que l’action posée était inutile, qu’elle n’est qu’une perte de temps, sans profit ni perte ».\
+(24)\
+(Q)Cogner des clous, c’est faire un travail répétitif.\
+(R)La réponse est FAUX. L’expression cogner des clous veut dire « dodeliner de la tête lorsqu’on s’endort involontairement en position assise ».\
+(25)\
+(Q)Un péteux de broue, c’est quelqu’un qui brise la fête, qui nuit aux réjouissances.\
+(R)La réponse est FAUX. Un péteux de broue est une personne vantarde, qui fanfaronne.\
+(26)\
+(Q)Se péter les bretelles, c’est avoir un excédent de poids et ne pas s’en soucier.\
+(R)La réponse est FAUX. L’expression se péter les bretelles est synonyme de « se vanter de ses exploits, faire de l’esbroufe ».\
+(27)\
+"
+
+exports.str=str
