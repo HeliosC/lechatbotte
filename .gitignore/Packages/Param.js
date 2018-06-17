6f4a31d3b442f900e27f65ca568a593f9e8ae58b
HeliosC <36132864+HeliosC@users.noreply.github.com>
2018-06-17 20:56:09 +0200

    Update Param.js

--- .gitignore/Packages/Param.js
+++ .gitignore/Packages/Param.js
@@ -17,6 +17,8 @@ const nomsub = "💕PUTAIN DE CHATONS"
 const nommodo = "🐾Chats sous chef"
 const nomadmin = "🦄Le Chat en chef"
 
+var Pclient
+
 tagS = "²"
 
 var message = function (msg) {
@@ -24,6 +26,8 @@ var message = function (msg) {
     BotReactions.message(msg)
     Connect4.message(msg)
     Quiz.message(msg)
+
+    testsleep(msg)
 }
 
 var messageReactionAdd = function (reaction, user) {
@@ -32,12 +36,45 @@ var messageReactionAdd = function (reaction, user) {
 }
 
 var setParam = function (client) {
+    Pclient = client
     BotReactions.setParam(chanIm, chanCh, nombot, nomadmin, nommodo, nomsub, nomdon, client, tagS)
     Queue.setParam(chanQ, nomadmin, nommodo)
     Connect4.setParam(client, chanJeux, nomadmin, nommodo)
-    Quiz.setParam(client,chanQuiz,nomadmin,nommodo)
+    Quiz.setParam(client, chanQuiz, nomadmin, nommodo)
 }
 
 exports.message = message
 exports.messageReactionAdd = messageReactionAdd
 exports.setParam = setParam
+
+var I = 0
+function test() {
+    setTimeout(() => {
+        //console.log("a")
+        I = I + 1
+        //client.channels.find('name', "testbotquiz").send("test " + I)
+        MSG.edit("test " + I)
+        test()
+    }, 60 * 1000);
+}
+
+function testsleep(msg) {
+    if (msg.content == "***bite***" && msg.author.username == "Helios" && msg.channel.name == "testbotquiz") {
+        testo = true
+        msg.channel.send("test 0")
+        test()
+    }
+
+    if (testo && msg.author.bot) {
+        MSG = msg
+        testo = false
+    }
+}
+
+function testsleepauto2() {
+    testo = true
+    Pclient.channels.find('name', "testbotquiz").send("test 0")
+    test()
+}
+
+exports.testsleepauto2 = testsleepauto2
