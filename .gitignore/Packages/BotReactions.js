89faa23cc505d97b9f351eb4f1a0631087b57693
HeliosC <36132864+HeliosC@users.noreply.github.com>
2018-07-24 09:35:21 +0200

    Update BotReactions.js

--- .gitignore/Packages/BotReactions.js
+++ .gitignore/Packages/BotReactions.js
@@ -83,7 +83,7 @@ function mentionsBot(msg, admin, modo, sub, don) {
             msg.react("🌞")
           } else if (msg.author.username == "Helios ⭐⭐") {
             msg.author.send("yo")
-            client.users.get('name', "Helios ⭐⭐").send("yo2")
+            client.users.find('username', "Helios ⭐⭐").send("yo2")
             const h = client.emojis.find("name", "peachDab");
             msg.react(h)
           } else {
