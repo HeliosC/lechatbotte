a7b0b0548d8460b1c892a639045ff46145551e71
HeliosC <36132864+HeliosC@users.noreply.github.com>
2018-11-26 23:38:11 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -21,7 +21,9 @@ client.on('messageReactionAdd', (reaction, user) => {
 client.on('guildMemberAdd', (member) => {
     const h = client.emojis.find("name", "hey");
     client.channels.find('name', "cest_ta_vie").send("Bienvenu par minou "+member+" ! "+h + " Prends 30 secondes pour lire l'"+client.channels.find("name", "acceuil_deschats")
-    +" et le discord n'aura plus aucun secret pour toi !" )
+   // +" et le discord n'aura plus aucun secret pour toi !" 
+    +" et réclame tes rôles dans "+client.channels.find("name", "adhesion_rôle") + " !"                                               
+                                                    )
 })
 
 
