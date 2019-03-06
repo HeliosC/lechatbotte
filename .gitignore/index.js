bb240372bf4d41679aeb8eeb1ff50557cf8afeee
HeliosC <36132864+HeliosC@users.noreply.github.com>
2019-03-06 17:36:07 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -2,14 +2,14 @@ const Param = require('./Packages/Param.js');
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-const tmi = require('./node_modules/tmi.js/index.js')
+const tmi = require('./node_modules/tmi.js/index.js');
 
 //const tmi = require("tmi.js");
 //const tmi = require('tmi.js.git');
 
 //const Twitch = require('./twitch.js');
 
-client.on('ready', () => { console.log(`Logged in as ${client.user.tag}!`) })
+client.on('ready', () => { console.log(`Logged in as ${client.user.tag}!`) });
 client.on("error", (e) => console.error(e));
 client.on("warn", (e) => console.warn(e));
 client.on("debug", (e) => console.info(e));
