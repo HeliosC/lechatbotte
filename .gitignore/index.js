f0d98f14424df3e31e68abcec4afd5f7c9e9b78a
HeliosC <36132864+HeliosC@users.noreply.github.com>
2019-03-06 14:08:39 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -2,7 +2,8 @@ const Param = require('./Packages/Param.js');
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-//const tmi = require('tmi.min.js');
+//const tmi = require('tmi.js');
+const tmi = require('tmi.js.git');
 
 //const Twitch = require('./twitch.js');
 
