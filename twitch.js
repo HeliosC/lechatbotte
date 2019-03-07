9115e77a24985bfe2ddae2ccc45b6d074926e0c9
HeliosProd <magicchris11@live.fr>
2019-03-07 23:57:42 +0100

    no message

--- twitch.js
+++ twitch.js
@@ -1,8 +1,4 @@
-
-
 const tmi = require('tmi.js')
-
-
 
 const tmiConfig = {
     options: {
@@ -51,14 +47,7 @@ hdb = "heliosdesbois"
 
 mods=["heliosdesbois","pouidesbois","chatdesbois","solis_the_sun"]
 
-
-
-// client.on('connected', (adress, port) => {
-//     console.log(client.getUsername() + " s'est connecté sur : " + adress + ", port : " + port);
-//     client.say(channel,"Kraoki, vous êtes en état d'arrestation !");
-// });
-
-
+client2.whisper(hdb, "Deployed");
 
 client.on("whisper", function (from, userstate, message, self) {
 
@@ -67,17 +56,6 @@ client.on("whisper", function (from, userstate, message, self) {
     if(message.toLowerCase().startsWith("zboub") && mods.indexOf(userstate['display-name'].toLowerCase())!=-1){
         client.say(hdb, "Sachez que j'adore le zboub")
         client2.say(hdb, "Sachez que j'adore le zboub")
-
-        console.log(userstate['display-name'])
-        console.log( mods.indexOf(userstate['display-name'].toLowerCase())!=-1 )
-
-
-        // console.log("test")
-        // //console.log(userstate)
-        //console.log(""+client.isMod("#heliosdesbois", "HeliosDesBois"))
-
-        // console.log(client.getChannels())
-
     }
 
     if(message.toLowerCase().startsWith("say ") && userstate['display-name'].toLowerCase() == hdb ){
@@ -99,39 +77,26 @@ client.on('chat', (channel, user, message, isSelf) => {
 
     m=message.toLowerCase()
 
-    if(message.toLowerCase().startsWith("arretez")){
+    if(m.startsWith("arretez")&&0){
         let words = message.split(" ")
         client.say(channel, words[1]+", vous êtes en état d'arrestation !");
     }
-
     if(m.indexOf("je peut")!=-1 || m.indexOf("tu peut")!=-1){// || m.indexOf("je peu")!=-1 || m.indexOf("tu peu")!=-1){
         client.say(channel, user['display-name']+" peuX");
     }
-
     if(m.indexOf("on peux")!=-1){
         client.say(channel, user['display-name']+" peuT");
     }
 
-
-
 });
 
 client2.on("subscription", (channel, username, method, message, userstate) => {
-    // Do your stuff.
     if(channel == "#"+cdb){
         client2.say(channel, username+" chatdeLove chatdeLove chatdeLove chatdeLove chatdeLove")
     }
 });
-
 client2.on("resub", (channel, username, months, message, userstate, methods) => {
-    // Do your stuff.
-    //let cumulativeMonths = ~~userstate["msg-param-cumulative-months"];
     if(channel == "#"+cdb){
         client2.say(channel, username+" chatdeLove chatdeLove chatdeLove chatdeLove chatdeLove")
     }
 });
-
-//user['display-name']
-
-//pas f, mais lol ow brite
-
