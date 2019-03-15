4da2664d246bad355add1288c12628ca487f176e
HeliosProd <magicchris11@live.fr>
2019-03-15 11:13:40 +0100

    no message

--- index.js
+++ index.js
@@ -9,3 +9,15 @@ lechatbotte.start()
 twitchBotHeliosDesBois.start()
 twitchBotPoliceDesBois.start()
 twitchBotPouiDesBois.start()
+
+var client = require('redis').createClient(process.env.REDIS_URL);
+client.on('connect', function() {
+    console.log('connected');
+});
+
+
+client.set('key', 'value');
+
+client.get('key', function(err, reply) {
+    console.log(reply); // "value"
+});
