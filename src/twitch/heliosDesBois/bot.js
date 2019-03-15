5082538751082df67815a23128eee3b30d706b82
HeliosProd <magicchris11@live.fr>
2019-03-15 22:27:25 +0100

    no message

--- src/twitch/heliosDesBois/bot.js
+++ src/twitch/heliosDesBois/bot.js
@@ -11,18 +11,30 @@ function startBot() {
         console.log(`${tmiConfig.identity.username} logged in on twitch !`)
     });
 
+    
 
     client.on("subscription", (channel, username, method, message, userstate) => {
         if(channel == "#" + cdb){
             client.say(channel, username + " chatdeLove chatdeLove chatdeLove chatdeLove chatdeLove");
         }
     });
-
     client.on("resub", (channel, username, months, message, userstate, methods) => {
+        if(channel == "#" + cdb){
+            client.say(channel, username + " chatdeLove chatdeLove chatdeLove chatdeLove chatdeLove");
+        }        
+    });
+    client.on("subgift", (channel, username, streakMonths, recipient, methods, userstate) => {
+        if(channel == "#" + cdb){
+            client.say(channel, username + " chatdeLove chatdeLove chatdeLove chatdeLove chatdeLove");
+        }        
+    });
+    client.on("submysterygift", (channel, username, numbOfSubs, methods, userstate) => {
         if(channel == "#" + cdb){
             client.say(channel, username + " chatdeLove chatdeLove chatdeLove chatdeLove chatdeLove");
         }
     });
+    
+
 }
 
 module.exports.start = startBot;
