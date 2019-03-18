bb03c8867de16f2a9ef954731dfed7f4e4d72127
HeliosProd <magicchris11@live.fr>
2019-03-18 21:17:59 +0100

    no message

--- src/twitch/pouiDesBois/bot.js
+++ src/twitch/pouiDesBois/bot.js
@@ -14,22 +14,22 @@ function startBot() {
 
     client.on("subscription", (channel, username, method, message, userstate) => {
         if(channel == "#" + cdb){
-            client.say(channel, username + " chatdeLove chatdeLove chatdeLove chatdeLove chatdeLove");
+            client.say(channel, username + " chatdeLove chatdeLove chatdeLove");
         }
     });
     client.on("resub", (channel, username, months, message, userstate, methods) => {
         if(channel == "#" + cdb){
-            client.say(channel, username + " chatdeLove chatdeLove chatdeLove chatdeLove chatdeLove");
+            client.say(channel, username + " chatdeLove chatdeLove chatdeLove");
         }        
     });
     client.on("subgift", (channel, username, streakMonths, recipient, methods, userstate) => {
         if(channel == "#" + cdb){
-            client.say(channel, username + " chatdeLove chatdeLove chatdeLove chatdeLove chatdeLove");
+            client.say(channel, username + " chatdeLove chatdeLove chatdeLove");
         }        
     });
     client.on("submysterygift", (channel, username, numbOfSubs, methods, userstate) => {
         if(channel == "#" + cdb){
-            client.say(channel, username + " chatdeLove chatdeLove chatdeLove chatdeLove chatdeLove");
+            client.say(channel, username + " chatdeLove chatdeLove chatdeLove");
         }
     });
 }
