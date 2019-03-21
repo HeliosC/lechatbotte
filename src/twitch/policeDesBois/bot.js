5e8626799286ec6f17e55503d2bc1c9ee53c4f61
HeliosProd <magicchris11@live.fr>
2019-03-21 13:13:44 +0100

    no message

--- src/twitch/policeDesBois/bot.js
+++ src/twitch/policeDesBois/bot.js
@@ -5,7 +5,7 @@ const tmiConfig = require("./config")
 const request = require('request')
 
 var redis = require('redis').createClient(process.env.REDIS_URL);
-redis.on('connect', function() {
+redis.on('connect', function () {
     console.log('connected');
 });
 
@@ -17,7 +17,7 @@ const cdb = "chatdesbois"
 const hdb = "heliosdesbois"
 
 const moderators = ["heliosdesbois", "pouidesbois", "chatdesbois", "solis_the_sun"]
-const joueursFortnite = ["toxiicdust","lhotzl","threshbard","tutofeeding","carottounet","vause","kraoki"]
+const joueursFortnite = ["toxiicdust", "lhotzl", "threshbard", "tutofeeding", "carottounet", "vause", "kraoki"]
 
 
 function startBot() {
@@ -56,151 +56,152 @@ function startBot() {
         let username = user.username
 
 
-/////////* Specific to chatDesBois's channel *//////////////////////////////////
+        /////////* Specific to chatDesBois's channel *//////////////////////////////////
 
-        var answer = ""
         if (channel.indexOf(cdb) != -1) { //return }
 
-        if (/(^|\W)(je|tu)\speu($|\W|t)/gmi.test(m)) {           //   je/tu peux
-            // client.say(channel, username + " peuX, l'orthographe veut ton bien-être !");
-            answer+=vide(answer)+"je/tu peuX"
-            }
+            if (username.toLowerCase() != hdb) {
 
-            if (/(^|\W)(il|elle|ont?)\speu($|\W|x)/gmi.test(m)) {               //   on peut
-            // client.say(channel, username + " peuT, l'orthographe veut ton bien-être !");
-            answer+=vide(answer)+"on peuT"
-            }
+                var answer = ""
 
-            if (/(^|\W)(je|tu)\sveu($|\W|t)/gmi.test(m)) {          //   je/tu veux
-            // client.say(channel, username + " veuX, l'orthographe veut ton bien-être !");
-            answer+=vide(answer)+"je/tu veuX"
-            }
 
-            if (/(^|\W)(il|elle|ont?)\sveu($|\W|x)/gmi.test(m)) {               //   on veut
-            // client.say(channel, username + " veuT, l'orthographe veut ton bien-être !");
-            answer+=vide(answer)+"on veuT"
-            }
-
-            if( answer!=""){ answer+=" , l'orthographe veut ton bien-être !"}
-
-            if (/(^|\W)sa\s?va($|\W)/gmi.test(m)) {                 //   sava
-            // client.say(channel, username + " *ça va, l'orthographe est ton ami, l'ami !");
-            answer+=vide(answer)+"*ça va, l'orthographe est ton ami, l'ami !"
-            }
-
-            if (/(^|\W)au final($|\W)/gmi.test(m)) {                 //   au final
-            //client.say(channel, username + " *finalement ! Tout doux avec la grammaire ! http://www.academie-francaise.fr/au-final ");
-            answer+=vide(answer)+"*finalement ! Tout doux avec la grammaire ! http://www.academie-francaise.fr/au-final ."
-            }
-
-            if (/(^|\s)tu\s?(su(sse|se|ce|ss|ç|çe)|susses|suses)(\s|$)/gmi.test(m)) {                 //   au final
-                answer+=vide(answer)+"*tu suçes"
-            }
-            
-            if (!isModerateur(username) && /chatt?e\s?(des|dé|de|d)\s?(bois?|boa)/gmi.test(m)) {                 //   chattedesbois
-            redis.lrange('chattedesbois', 0, -1, function(err, reply) {
-                    if(reply.indexOf(username)==-1){
-                    // client.say(channel, username + " raté ! C'est \"chat des bois\", c'est pas si dur pourtant :) Next time, j'te goume !");
-                    answer+=vide(answer)+"raté ! C'est \"chat des bois\", c'est pas si dur pourtant :) Next time, j'te goume !"
-                    redis.rpush('chattedesbois',username)
-                    }else {
-                    client.say(channel, username + " je t'avais prévenu !");
-                    client.timeout(channel, username, 5)
-                    }
-            });
-            }
-
-//            &&( /(je|ont?)\s(peu.?|).{0,}(duo|squad|skad|jou(|.|..|...))\s?((a|e)ns|ave.\s?toi|\?)/gmi.test(m)  //ON PEUT JOUER ?   |$
-//jou(|.|..|...)
-
-            if ( !isModerateur(username) && (joueursFortnite.indexOf(username.toLowerCase()) == -1
-            && /can\s?i\s?pl..\s?wi..\s?(you|u)/gmi.test(m)){
-
-            }
-
-            if ( !isModerateur(username) && (joueursFortnite.indexOf(username.toLowerCase()) == -1)
-            &&( /(je|ont?).{0,}(duo|squad|skad)\s?((a|e)ns|ave.\s?toi|\?|$)/gmi.test(m)  //ON PEUT JOUER ?   |$
-            ||  /(je|ont?)\s(peu.?|pourr?ai.?)\s?jou(|er|é|es|e|et)\s?((a|e)ns|ave.\s?(toi|vou|vous)|\?)/gmi.test(m)
-            ||  /tu.{0,}jou(|.|..)\s?ave.\s?(moi|(t|tes|té|les|lé)\s?(vie|fol|abo))/gmi.test(m)
-            ||  /tu\s.{0,}(fait|fé|faire|fai|fais|fair)\s.{0,}(des|dé|d).{0,}gam.{0,}(vi(uv|ew|ev|ouv)eu?r|abo)/gmi.test(m)
-            ||  /can\s?i\s?pl..\s?wi..\s?(you|u)/gmi.test(m)
-            )
-            ) {
-                    console.log("bite")
-                    request(url+clientID, function (error, response, body) {
-                            if (!error && response.statusCode == 200) {
-                                    let data = JSON.parse(body)
-                                    console.log(data.game)
-                                    if(data.game.toLowerCase() == "fortnite"  ){
-                                            console.log("bite2")
-                                            // client.say(channel,"Pas de games viewers sur Fortnite ! Mais sur d'autres jeux ça sera avec plaisir !")
-                                            if( /can\s?i\s?pl..\s?wi..\s?(you|u)/gmi.test(m) ){
-                                                answer+=vide(answer)+"chatdesbois doesn't play with the viouveurs !"
-                                            }else{
-                                                answer+=vide(answer)+"pas de games viewers sur Fortnite ! Mais sur d'autres jeux ça sera avec plaisir !"
-                                            }
-                                            onAnswer(answer)
-                                    }else{ onAnswer(answer) }
-                            } else {
-                                    console.error("unable ")
-                            }
-                    })
-            }else{
-                onAnswer(answer)
-            }
-
-            function vide(answer){
-                    return answer == "" ? "" : " Et "
-            }
-
-            function onAnswer(answer){
-                if(answer!=""){
-                        client.say(channel,username+" "+answer)
+                if (/(^|\W)(je|tu)\speu($|\W|t)/gmi.test(m)) {           //   je/tu peux
+                    // client.say(channel, username + " peuX, l'orthographe veut ton bien-être !");
+                    answer += vide(answer) + "je/tu peuX"
                 }
-            }
 
+                if (/(^|\W)(il|elle|ont?)\speu($|\W|x)/gmi.test(m)) {               //   on peut
+                    // client.say(channel, username + " peuT, l'orthographe veut ton bien-être !");
+                    answer += vide(answer) + "on peuT"
+                }
 
+                if (/(^|\W)(je|tu)\sveu($|\W|t)/gmi.test(m)) {          //   je/tu veux
+                    // client.say(channel, username + " veuX, l'orthographe veut ton bien-être !");
+                    answer += vide(answer) + "je/tu veuX"
+                }
 
+                if (/(^|\W)(il|elle|ont?)\sveu($|\W|x)/gmi.test(m)) {               //   on veut
+                    // client.say(channel, username + " veuT, l'orthographe veut ton bien-être !");
+                    answer += vide(answer) + "on veuT"
+                }
 
-        if (/^!massacre\s?\+\s?1$/gmi.test(m)) { //*massacre -> incremente
-            //massacres += 1;
-            redis.incr('massacres', function(err, reply) {
-                afficheMassacres(client, channel, parseInt(reply));
-            });
+                if (answer != "") { answer += " , l'orthographe veut ton bien-être !" }
 
-        } else if (/^!massacre$/gmi.test(m)) { //*massacres -> affiche le nb
-            redis.get('massacres', function(err, reply) {
-                afficheMassacres(client, channel, parseInt(reply));
-            });
+                if (/(^|\W)sa\s?va($|\W)/gmi.test(m)) {                 //   sava
+                    // client.say(channel, username + " *ça va, l'orthographe est ton ami, l'ami !");
+                    answer += vide(answer) + "*ça va, l'orthographe est ton ami, l'ami !"
+                }
 
-        }else if (isModerateur(user.username) && /^!massacre \d/gmi.test(m)) {
-            massacres = parseInt(m.slice(9+1)) || 0;
-            afficheMassacres(client, channel, massacres);
-            redis.set('massacres', massacres);
-        }
+                if (/(^|\W)au final($|\W)/gmi.test(m)) {                 //   au final
+                    //client.say(channel, username + " *finalement ! Tout doux avec la grammaire ! http://www.academie-francaise.fr/au-final ");
+                    answer += vide(answer) + "*finalement ! Tout doux avec la grammaire ! http://www.academie-francaise.fr/au-final ."
+                }
 
-        // if (m.startsWith("!game")) {
-        //     client.say(channel, "Chatdesbois ne fait pas de games viewers sur Fortnite");
-        // }
+                if (/(^|\s)tu\s?(su(sse|se|ce|ss|ç|çe)|susses|suses)(\s|$)/gmi.test(m)) {                 //   au final
+                    answer += vide(answer) + "*tu suçes"
+                }
 
-        if (m.startsWith("arretez")) {
-            console.log(channel)
-            request('https://tmi.twitch.tv/group/user/'+channel.slice(1)+'/chatters', function (error, response, body) {
-                if (!error && response.statusCode == 200) {
-                    let data = JSON.parse(body)
-                    let viewers = Object.values(data.chatters).reduce((accumulator, array) => accumulator.concat(array), [])
-                    let words = message.split(" ")
-                    if(words.length > 0 ){
-                        let word = words[1]
-                        if( isModerateur(username) || (word.toLowerCase()!="policedesbois" && word.toLowerCase()!="heliosdesbois" && viewers.indexOf(word.toLowerCase())!=-1) ){
-                            client.say(channel, word + ", vous êtes en état d'arrestation !");
+                if (!isModerateur(username) && /chatt?e\s?(des|dé|de|d)\s?(bois?|boa)/gmi.test(m)) {                 //   chattedesbois
+                    redis.lrange('chattedesbois', 0, -1, function (err, reply) {
+                        if (reply.indexOf(username) == -1) {
+                            // client.say(channel, username + " raté ! C'est \"chat des bois\", c'est pas si dur pourtant :) Next time, j'te goume !");
+                            answer += vide(answer) + "raté ! C'est \"chat des bois\", c'est pas si dur pourtant :) Next time, j'te goume !"
+                            redis.rpush('chattedesbois', username)
+                        } else {
+                            client.say(channel, username + " je t'avais prévenu !");
+                            client.timeout(channel, username, 5)
                         }
-                    }
-                } else {
-                    console.error("unable ")
+                    });
                 }
-            })
-        }
+
+                //            &&( /(je|ont?)\s(peu.?|).{0,}(duo|squad|skad|jou(|.|..|...))\s?((a|e)ns|ave.\s?toi|\?)/gmi.test(m)  //ON PEUT JOUER ?   |$
+                //jou(|.|..|...)
+
+
+                if (!isModerateur(username) && (joueursFortnite.indexOf(username.toLowerCase()) == -1)
+                    && (/(je|ont?).{0,}(duo|squad|skad)\s?((a|e)ns|ave.\s?toi|\?|$)/gmi.test(m)  //ON PEUT JOUER ?   |$
+                        || /(je|ont?)\s(peu.?|pourr?ai.?)\s?jou(|er|é|es|e|et)\s?((a|e)ns|ave.\s?(toi|vou|vous)|\?)/gmi.test(m)
+                        || /tu.{0,}jou(|.|..)\s?ave.\s?(moi|(t|tes|té|les|lé)\s?(vie|fol|abo))/gmi.test(m)
+                        || /tu\s.{0,}(fait|fé|faire|fai|fais|fair)\s.{0,}(des|dé|d).{0,}gam.{0,}(vi(uv|ew|ev|ouv)eu?r|abo)/gmi.test(m)
+                        || /can\s?i\s?pl..\s?wi..\s?(you|u)/gmi.test(m)
+                    )
+                ) {
+                    console.log("bite")
+                    request(url + clientID, function (error, response, body) {
+                        if (!error && response.statusCode == 200) {
+                            let data = JSON.parse(body)
+                            console.log(data.game)
+                            if (data.game.toLowerCase() == "fortnite") {
+                                console.log("bite2")
+                                // client.say(channel,"Pas de games viewers sur Fortnite ! Mais sur d'autres jeux ça sera avec plaisir !")
+                                if (/can\s?i\s?pl..\s?wi..\s?(you|u)/gmi.test(m)) {
+                                    answer += vide(answer) + "chatdesbois doesn't play with the viouveurs !"
+                                } else {
+                                    answer += vide(answer) + "pas de games viewers sur Fortnite ! Mais sur d'autres jeux ça sera avec plaisir !"
+                                }
+                                onAnswer(answer)
+                            } else { onAnswer(answer) }
+                        } else {
+                            console.error("unable ")
+                        }
+                    })
+                } else {
+                    onAnswer(answer)
+                }
+
+                function vide(answer) {
+                    return answer == "" ? "" : " Et "
+                }
+
+                function onAnswer(answer) {
+                    if (answer != "") {
+                        client.say(channel, username + " " + answer)
+                    }
+                }
+
+            }
+
+
+
+            if (/^!massacre\s?\+\s?1$/gmi.test(m)) { //*massacre -> incremente
+                //massacres += 1;
+                redis.incr('massacres', function (err, reply) {
+                    afficheMassacres(client, channel, parseInt(reply));
+                });
+
+            } else if (/^!massacre$/gmi.test(m)) { //*massacres -> affiche le nb
+                redis.get('massacres', function (err, reply) {
+                    afficheMassacres(client, channel, parseInt(reply));
+                });
+
+            } else if (isModerateur(user.username) && /^!massacre \d/gmi.test(m)) {
+                massacres = parseInt(m.slice(9 + 1)) || 0;
+                afficheMassacres(client, channel, massacres);
+                redis.set('massacres', massacres);
+            }
+
+            // if (m.startsWith("!game")) {
+            //     client.say(channel, "Chatdesbois ne fait pas de games viewers sur Fortnite");
+            // }
+
+            if (m.startsWith("arretez")) {
+                console.log(channel)
+                request('https://tmi.twitch.tv/group/user/' + channel.slice(1) + '/chatters', function (error, response, body) {
+                    if (!error && response.statusCode == 200) {
+                        let data = JSON.parse(body)
+                        let viewers = Object.values(data.chatters).reduce((accumulator, array) => accumulator.concat(array), [])
+                        let words = message.split(" ")
+                        if (words.length > 0) {
+                            let word = words[1]
+                            if (isModerateur(username) || (word.toLowerCase() != "policedesbois" && word.toLowerCase() != "heliosdesbois" && viewers.indexOf(word.toLowerCase()) != -1)) {
+                                client.say(channel, word + ", vous êtes en état d'arrestation !");
+                            }
+                        }
+                    } else {
+                        console.error("unable ")
+                    }
+                })
+            }
         }
     });
 }
@@ -227,7 +228,7 @@ function heure() {
     if (minutes < 10) {
         minutes = "0" + minutes;
     }
-    return date.getDate() + ":" + (date.getMonth()+1) + " " + (heure+1) + "h" + minutes;
+    return date.getDate() + ":" + (date.getMonth() + 1) + " " + (heure + 1) + "h" + minutes;
 }
 
 
