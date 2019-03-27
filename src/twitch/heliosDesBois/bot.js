02be2ec79c006e22469af1618460ba78ccb17f78
HeliosProd <magicchris11@live.fr>
2019-03-27 20:51:55 +0100

    enlever les coeurs en react

--- src/twitch/heliosDesBois/bot.js
+++ src/twitch/heliosDesBois/bot.js
@@ -12,26 +12,26 @@ function startBot() {
     });
 
 
-    client.on("subscription", (channel, username, method, message, userstate) => {
-        if(channel == "#" + cdb){
-            client.say(channel, username + " chatdeLove chatdeLove chatdeLove");
-        }
-    });
-    client.on("resub", (channel, username, months, message, userstate, methods) => {
-        if(channel == "#" + cdb){
-            client.say(channel, username + " chatdeLove chatdeLove chatdeLove");
-        }        
-    });
-    // client.on("subgift", (channel, username, streakMonths, recipient, methods, userstate) => {
+    // client.on("subscription", (channel, username, method, message, userstate) => {
+    //     if(channel == "#" + cdb){
+    //         client.say(channel, username + " chatdeLove chatdeLove chatdeLove");
+    //     }
+    // });
+    // client.on("resub", (channel, username, months, message, userstate, methods) => {
     //     if(channel == "#" + cdb){
     //         client.say(channel, username + " chatdeLove chatdeLove chatdeLove");
     //     }        
     // });
-    client.on("submysterygift", (channel, username, numbOfSubs, methods, userstate) => {
-        if(channel == "#" + cdb){
-            client.say(channel, username + " chatdeLove chatdeLove chatdeLove");
-        }
-    });
+    // // client.on("subgift", (channel, username, streakMonths, recipient, methods, userstate) => {
+    // //     if(channel == "#" + cdb){
+    // //         client.say(channel, username + " chatdeLove chatdeLove chatdeLove");
+    // //     }        
+    // // });
+    // client.on("submysterygift", (channel, username, numbOfSubs, methods, userstate) => {
+    //     if(channel == "#" + cdb){
+    //         client.say(channel, username + " chatdeLove chatdeLove chatdeLove");
+    //     }
+    // });
 }
 
 module.exports.start = startBot;
