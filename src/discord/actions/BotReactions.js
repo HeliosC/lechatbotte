4df4bbb6d102bdaa9e31a3015211307fddd0aae9
HeliosProd <magicchris11@live.fr>
2019-05-05 17:42:42 +0200

    add tenor

--- src/discord/actions/BotReactions.js
+++ src/discord/actions/BotReactions.js
@@ -189,8 +189,11 @@ BotReactions.prototype.checkImageToMove = function(message) {
 
   if (message.channel.name != this.channels.chanCh) return triggeredAction;
 
-  if (/(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|jpeg|gif|png)/.test(message.content)
-  ||/https?:\/\/giphy.com\/gifs\/.+/.test(message.content)) {
+  if (/(http(s?):)([/|.|\w|\s|-])*\(.|-)(?:jpg|jpeg|gif|png)/.test(message.content)
+  ||/https?:\/\/tenor.com\/view\/.+/.test(message.content)
+  ||/https?:\/\/giphy.com\/gifs\/.+/.test(message.content)
+  ||/https?:\/\/gfycat.com\/.+/.test(message.content)
+  ){
     message.channel.send(message.author + " : " + this.botClient.channels.find(c => c.name == this.channels.images));
     let imageChannel = this.botClient.channels.find(c => c.name == this.channels.images);
     imageChannel.send(this.botClient.channels.find(c => c.name == this.channels.chanCh) + "\n" + message.author + " : " + message.content);
