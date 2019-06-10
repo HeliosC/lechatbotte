c4137a163ebceb0db6d07aa01e99331195e5c857
theipple <alex.ipple@gmail.com>
2019-06-10 18:09:24 +0200

    use varibale to set the port for express

--- src/web/app.js
+++ src/web/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
-const port = 80;
+const port = process.env.PORT || 5000;
 
 app.use(express.static('public'));
 
