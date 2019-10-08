0c89715baa6f5602b34e7975be5cead5de6c18a7
HeliosProd <magicchris11@live.fr>
2019-10-08 17:23:28 +0200

    no message

--- src/twitch/policeDesBois/bot.js
+++ src/twitch/policeDesBois/bot.js
@@ -848,7 +848,7 @@ function onCommand(client, m, user, date, mode){
 
 function onTop(client, top){
     classement = top != ''? 'GLOBAL' : 'MENSUEL'
-    client.say(cdb, 'Qui est devant toi dans le classement MENSUEL? Des genoux à casser ? La réponse ici ! ➡️ http://top.chatdesbois.stream/'+top
+    client.say(cdb, 'Qui est devant toi dans le classement MENSUEL? Des genoux à casser ? La réponse ici ! ➡️ http://top.chatdesbois.stream/'+top)
     //Qui est devant toi dans le classement ' + classement + '? Des genoux à casser ? La réponse ici : >>> '
     // +'chatdesbois.herokuapp.com/'+top
     //+'http://top.chatdesbois.stream/'+top
