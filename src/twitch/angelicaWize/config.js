72645ba6ea421e78ce347cecf132d66dfb42365f
HeliosProd <magicchris11@live.fr>
2019-10-09 23:58:43 +0200

    no message

---
+++ src/twitch/angelicaWize/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    options: {
+        debug: false
+    },
+    connection: {
+        reconnect:  true
+    },
+    identity: {
+        username: "AngelicaWize",
+        password: process.env.angelica
+    },
+    channels: [
+        "poulpita",
+        "heliosdesbois"
+    ]
+};
+  
