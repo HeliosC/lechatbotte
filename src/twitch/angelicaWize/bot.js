843f4729aac96e8e8e10ceedaab30eb8e57e14bc
HeliosProd <magicchris11@live.fr>
2019-10-09 23:37:39 +0200

    no message

--- src/twitch/angelicaWize/bot.js
+++ src/twitch/angelicaWize/bot.js
@@ -10,21 +10,12 @@ function startBot() {
         console.log(`${tmiConfig.identity.username} logged in on twitch !`)
     });
 
-
     client.on('chat', (channel, user, m, isSelf) => {
-
         if(isSelf){ return; }
-
-        
-
         if(/( |^)pardon( |$)/gmi.test(m) && !/(oh|ho) pardon/gmi.test(m)){
             client.say(channel, "Oh pardon*!")
         }
-
     })
-
 }
-
-
 
 module.exports.start = startBot;
