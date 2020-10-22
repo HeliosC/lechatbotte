88f6e12de46304150cb1b94c0177087848a92373
Christophe Sikes <sikes_c@etna-alternance.net>
2020-10-22 13:31:18 +0200

    no message

--- src/twitch/policeDesBois/api_twitch.js
+++ src/twitch/policeDesBois/api_twitch.js
@@ -8,11 +8,11 @@ const oauth = process.env.oauthhelios
 //     lageekenrose : process.env.SheetLGER
 // }
 const Sheets = {
-    49041281 : process.env.SheetKrao,
-    122699636 : process.env.SheetCDB,
-    185261350 : process.env.SheetWillo,
-    204501281 : process.env.SheetPoulpi,
-    112826022 : process.env.SheetLGER,
+    // 49041281 : process.env.SheetKrao,
+    // 122699636 : process.env.SheetCDB,
+    // 185261350 : process.env.SheetWillo,
+    // 204501281 : process.env.SheetPoulpi,
+    // 112826022 : process.env.SheetLGER,
     159948000 : process.env.SheetCatnip
 }
 
